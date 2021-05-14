5096a81406f44bd97e1a32ef2b938148472c4c62
Timoffey <videotimoffey@gmail.com>
2021-05-14 19:04:21 +0700

    первый экран выполнен

--- src/index.js
+++ src/index.js
@@ -1,2 +1,4 @@
 import './styles/style.styl'
+import smoothscroll from 'smoothscroll-polyfill';
+smoothscroll.polyfill();
 import './js/range.js'
--- src/js/range.js
+++ src/js/range.js
@@ -3,15 +3,30 @@ range.value = 0;
 range.addEventListener('input', (e) => {
   const pos = e.target.value
   range.style.background = `linear-gradient(90deg, #FF9796 0%,#FE4D4A ${pos / 500}%,#E2E2E2 0%)`
+
   const tooltip = document.querySelector('.slider__tooltip');
   const rangeWidth = parseInt(getComputedStyle(e.target).width, 10)
-  tooltip.innerText = pos + '₽'
+  tooltip.innerText = Number(pos).toLocaleString() + ' ₽'
   tooltipWidth = parseInt(getComputedStyle(tooltip).width)
-  tooltip.style.left = `${(rangeWidth - 25) / 50000 * pos + 25 - tooltipWidth / 2}px` // Корректируем ширину слайдера на ширину пипки
-
+  tooltip.style.left = `${(rangeWidth - 25) / 50000 * pos + 33 - tooltipWidth / 2}px` // Корректируем ширину слайдера на ширину пипки
 
   const root = document.querySelector(":root");
   root.style.setProperty("--slider-tooltip-arr", tooltipWidth / 2 + 5 + 'px');
 
   tooltip.style.visibility = pos == 0 ? 'hidden' : 'visible'
+})
+
+
+range.addEventListener('change', () => {
+  const summ = Number(document.querySelector('.slider__range').value).toLocaleString();
+  const finalScreen = document.querySelector('.final-screen')
+
+  finalScreen.style.display = 'block'
+  finalScreen.scrollIntoView({ behavior: 'smooth' })
+
+  document.querySelector('.final-screen__title').innerText = `Вы откладываете ${summ} ₽ в месяц.\nЗа три года вы бы заработали:`
+  document.querySelector('invest-types--s')
+  test()
+
+
 })
